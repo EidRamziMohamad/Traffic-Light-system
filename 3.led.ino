8804e42d1987c0ac176fab5f65d4ff08f27e9262
EidRamziMohamad <139391080+EidRamziMohamad@users.noreply.github.com>
2023-07-14 02:13:39 +0300

    Add files via upload

---
+++ 3.led.ino
@@ -0,0 +1,205 @@
+// C++ code
+//
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int d =500;
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,0);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,1);
+delay(d);
+
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,1);
+delay(d);
+ 
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,0);
+digitalWrite(5,1);
+  
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,0);
+digitalWrite(5,1);
+  
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,0);
+ digitalWrite(5,1); 
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,0);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,0);
+digitalWrite(4,1);
+delay(d);
+digitalWrite(1,0);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+
+digitalWrite(1,0);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,0);
+digitalWrite(3,1);
+digitalWrite(4,0);
+  digitalWrite(5,1);
+delay(d);
+digitalWrite(1,1);
+digitalWrite(2,1);
+digitalWrite(3,1);
+digitalWrite(4,1);
+  digitalWrite(5,1);
+delay(d);
+  
+  
+  
+  
+}
